8621d5352312d26c818aef5bdb5b70051e69f8e9
Petr Kočí <petr.koci@samizdat.cz>
2024-04-04 18:31:07 +0200

    get url parameter

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,11 @@
 import { useState } from 'preact/hooks'
 
-export function App() {
+export function App({ chart }: { chart: string | null }) {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="underline text-lg">hoj</h1>
+      <h1 className="underline text-lg">Graf: {chart}</h1>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,10 @@ import { render } from 'preact'
 import { App } from './app.tsx'
 import './index.css'
 
-render(<App />, document.getElementById('app')!)
+function getURLParameter(name: string): string | null {
+    return new URLSearchParams(window.location.search).get(name);
+}
+
+let chart = getURLParameter('chart');
+
+render(<App chart={chart} />, document.getElementById('app')!)
