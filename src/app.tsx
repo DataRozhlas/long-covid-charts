4bbfe7ca35403d0206f4cf68bf9f4eb14482bea2
Petr Kočí <petr.koci@samizdat.cz>
2024-04-04 15:28:30 +0200

    boilerplate

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'preact/hooks'
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="underline text-lg">hoj</h1>
+    </>
+  )
+}
